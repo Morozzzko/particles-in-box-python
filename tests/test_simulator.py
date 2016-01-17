236db46df3d294488a3fbafd45facbee41e0b26f
Michael Orlov <overdzen@gmail.com>
2016-01-17 19:05:35 +0300

    fix: изменены тесты для класса BinarySimulator

--- tests/test_simulator.py
+++ tests/test_simulator.py
@@ -6,9 +6,11 @@ import unittest
 from struct import *
 from math import floor
 import tempfile
+from os import path
 
 
 class TestNewSimulation(unittest.TestCase):
+
     def setUp(self):
         self.v_init = 3.0
         self.n_left = 100
@@ -27,9 +29,31 @@ class TestNewSimulation(unittest.TestCase):
                                    n_left=self.n_left,
                                    n_right=self.n_right,
                                    v_init=self.v_init)
+        self.directory = tempfile.TemporaryDirectory()
+        self.path = path.join(self.directory.name.__str__(), 'fooo.txt')
+        file = open(self.path, 'bw+')
+        self.parameters = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 1, 1, 12.0, 13.0]
+        self.particle_arrays = [[1.11, 2.22, 3.33, 4.44, 1],
+                                [5.55, 6.66, 7.77, 8.88, 2],
+                                [9.99, 10.10, 11.11, 12.12, 1],
+                                [13.13, 14.14, 15.15, 16.16, 2],
+                                [1.11, 2.22, 3.33, 4.44, 1],
+                                [3, 6.66, 7.77, 8.88, 2]]
+        file.write(pack(BinarySimulator.str_decode, *self.parameters))
+        for particle in self.particle_arrays:
+            file.write(pack(Particle.str_decode, *particle))
+        file.close()
 
     def tearDown(self):
         pass
+
+    def get_simulator_parameters(self, simulator):
+        return (simulator.box_width, simulator.box_height,
+                simulator.delta_v_top, simulator.delta_v_bottom,
+                simulator.delta_v_side, simulator.barrier_x,
+                simulator.barrier_width, simulator.hole_y,
+                simulator.hole_height, simulator.v_loss,
+                simulator.particle_r, len(simulator.particles), simulator.g)
 
     def test_initial_distribution_v_init(self):
         for particle in self.simulator.particles:
@@ -67,27 +91,17 @@ class TestNewSimulation(unittest.TestCase):
         num_of_states = sum([1 for state in simulation])
         self.assertEqual(num_of_states, floor(num_of_sec * num_of_snap) + 1)
 
-    def test_bin_simulator(self):
-        directory = tempfile.TemporaryDirectory()
-        filename = ''.join([directory.name, '/fooo.txt'])
-        file = open(filename, 'bw+')
-        parameters = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 1, 1, 12, 13]
-        particle_arrays = [[1.11, 2.22, 3.33, 4.44, 1],
-                           [5.55, 6.66, 7.77, 8.88, 2],
-                           [9.99, 10.10, 11.11, 12.12, 1],
-                           [13.13, 14.14, 15.15, 16.16, 2],
-                           [1.11, 2.22, 3.33, 4.44, 1],
-                           [3, 6.66, 7.77, 8.88, 2]]
-        file.write(pack(BinarySimulator.str_decode, *parameters))
-        for particle in particle_arrays:
-            file.write(pack(Particle.str_decode, *particle))
-        file.close()
-        simulator = BinarySimulator(filename)
-        i = len(particle_arrays) - len(simulator.particles)
+    def test_binary_simulator_init(self):
+        simulator = BinarySimulator(self.path)
+        params = tuple(self.parameters[:11] + [self.parameters[11] + self.parameters[12], self.parameters[14]])
+        self.assertEqual(params, self.get_simulator_parameters(simulator))
+
+    def test_binary_simulator_next_state(self):
+        simulator = BinarySimulator(self.path)
+        i = len(self.particle_arrays) - len(simulator.particles)
         simulator.next_state()
         simulator.next_state()
         for particle in simulator.particles:
-            p = Particle(particle_arrays[i][-1], *particle_arrays[i][:-1])
+            p = Particle(self.particle_arrays[i][-1], *self.particle_arrays[i][:-1])
             self.assertEqual(particle, p)
             i += 1
-        file.close()
